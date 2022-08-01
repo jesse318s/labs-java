1e0e3ad1625441cf13d584a5f933139618acc38f
Jesse Sites <85454439+jesse318s@users.noreply.github.com>
2022-08-01 02:45:36 -0500

    Delete PracticeProject directory

--- PracticeProject/src/MainClass.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-public class MainClass {
-    public static void main(String[] args) {
-        // Hello World
-        JOptionPane.showMessageDialog(null,"Hello World!");
-        System.out.println("Hello World!");
-    }
-}
